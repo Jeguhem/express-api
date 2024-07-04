8787a39239266aabcefc17a96fbd448b5fbe4e2e
Omotowa Oluwafemi <oluwafemisamuel2004@gmail.com>
2024-07-04 17:32:17 +0100

    changed location

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ app.get("/api/hello", async (req, res) => {
   }
 
   try {
-    // const clientIp =
-    //   req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    // const myIP = clientIp.includes(":") ? clientIp.split(":").pop() : clientIp;
+    // const response = await axios.get("https://api.ipify.org?format=json");
+    // const Ip_data = response.data;
+    // let myIP = Ip_data.ip;
 
-    // console.log(myIP);
-    const response = await axios.get("https://api.ipify.org?format=json");
-    const Ip_data = response.data;
-    let myIP = Ip_data.ip;
+    const response = await fetch("https://api.ipify.org?format=json");
+    const ip_data = await response.json();
+    let myIP = ip_data.ip;
 
     const Location = await axios.get("https://ipapi.co/" + myIP + "/json/");
     const data = Location.data;
